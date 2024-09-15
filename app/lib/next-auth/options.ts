0b7a393d8628e2cbcdb626d74b2e8255c1693984
hayato_sawamura <hayato.oorer1018@gmail.com>
2024-09-15 21:24:34 +0900

    Prismaの初期設定、OAuth設定の追加

---
+++ app/lib/next-auth/options.ts
@@ -0,0 +1,26 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import prisma from "../prisma";
+
+export const nextAuthOptions: NextAuthOptions = {
+    debug: false,
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+        session: ({ session, user }) => {
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: user.id,
+                },
+            };
+        },
+    },
+};
