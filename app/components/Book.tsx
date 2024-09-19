b9745de7e2f38fe4065d1d0fd12939ff108454d9
hayato_sawamura <hayato.oorer1018@gmail.com>
2024-09-19 23:21:33 +0900

    不要な説明部分を削除、imageをレスポンシブ化

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -95,11 +95,11 @@ const Book = ({ book, isPurchased, user }: BookProps) => {
                         alt={book.title}
                         width={450}
                         height={350}
+                        layout="responsive"
                         className="rounded-t-md"
                     />
                     <div className="px-4 py-4 bg-slate-100 rounded-b-md">
                         <h2 className="text-lg font-semibold">{book.title}</h2>
-                        <p className="mt-2 text-lg text-slate-600">この本は○○...</p>
                         <p className="mt-2 text-md text-slate-700">値段：{book.price}円</p>
                     </div>
                 </a>
