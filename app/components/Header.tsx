174da8d53fe06b8051ff548b9b9b1e3c04782046
hayato_sawamura <hayato.oorer1018@gmail.com>
2024-10-06 17:34:57 +0900

    ホームページ名を変更

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,7 +13,7 @@ const Header = async () => {
         <header className="bg-slate-600 text-gray-100 shadow-lg">
             <nav className="flex items-center justify-between p-4">
                 <Link href={"/"} className="text-xl font-bold">
-                    Book Commerce
+                    Keibayoso Commerce
                 </Link>
                 <div className="flex items-center gap-1">
                     <Link
